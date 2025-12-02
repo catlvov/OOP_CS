f53daf133b6c19b7a3fed05345fe6554fd0dc4dc
catlvov <kostalvov390@gmail.com>
2025-12-02 12:38:01 +0800

    A

---
+++ OOP_CS/Point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CS
+{
+    internal class Point
+    {
+        double x;
+        double y;
+        public double GetX()
+        {
+            return x;
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        //              metods:
+        public void Print()
+        {
+            Console.WriteLine($"x = {x} , y = {y}");
+        }
+        public double Distance(Point other)
+        {
+            double dx = other.X - this.X;
+            double dy = other.Y - this.Y;
+            return Math.Sqrt(dx * dx + dy);
+        }
+    }
+}
--- OOP_CS/Program.cs
+++ OOP_CS/Program.cs
@@ -10,6 +10,21 @@ namespace OOP_CS
     {
         static void Main(string[] args)
         {
+            Point A = new Point();
+            //Console.WriteLine($"{A.GetX()} {A.GetY()}");
+            //A.SetX(5);
+            //A.SetY(10.3);
+            A.X = 212.125; //свойства позволяют использовать инкопсуляцию как обычные переменые в классе
+            A.Y = 321.11;
+            A.Print();
+
+            Point B = new Point();
+            B.X = 100;
+            B.Y = 200;
+            B.Print();
+
+            double dist = A.Distance(B);
+            Console.WriteLine($"Дистанция от A до B: {dist}");
         }
     }
 }
